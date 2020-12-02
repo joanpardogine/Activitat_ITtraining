3396831d45a853c1294f8522298d1b7db0bab0fb
joanpardogine <joanpardo@ginebro.cat>
2020-12-02 19:25:54 +0100

    Afegits classes i fer que 1a preg. s'amaga

--- js/preguntes.js
+++ js/preguntes.js
@@ -2,21 +2,31 @@ let arrRespostes = [], arrRespostesCorrectes = [];
 arrRespostesCorrectes=[1,3,4,2];
 
 function pregunta(objecte, numPreg){
-  console.log(" pos 0 = " + objecte.form[0].checked);
-  console.log(" pos 1 = " + objecte.form[1].checked);
-  console.log(" pos 2 = " + objecte.form[2].checked);
-  console.log(" pos 2 = " + objecte.form[3].checked);
-
-let resposta;
-  for (let i=0; i<4; i++){
-    if(objecte.form[i].checked){
-      resposta = i + 1 ;
+  let resposta;
+  let obj;
+    for (let i=0; i<4; i++){
+      if(objecte.form[i].checked){
+        resposta = i + 1 ; // + 1 => per començar des
+                           // de 1 i no des de 0
+      }
     }
-  }
-  arrRespostes[numPreg] = resposta;
-  
-  alert(" has escollit la resposta " + arrRespostes[numPreg]);
+    arrRespostes[numPreg] = resposta;
+    console.log("arrRespostes[" + numPreg + "] = " + arrRespostes[numPreg]);
+    alert(" has escollit la resposta " + arrRespostes[numPreg]);
+    switch(numPreg) {
+      case 0:
+        obj = document.getElementById("pregunta-1");
+        obj.classList.remove("elementVisible");
+        obj.classList.add("elementOcult");
+        obj = document.getElementById("pregunta-2");
+        obj.classList.remove("elementOcult");
+        obj.classList.add("elementVisible");
+        break;
+    case 1:
+      alert("hola!");
+        break;
+      }
 
-  
-  
-}
+
+}  // FINAL   function pregunta()
+
